7468fe673410d3f11cb62fa23db91936e36b574a
Đạt <datlowashere@gmail.com>
2023-10-26 21:31:15 +0700

    Đạt update đổi mật khẩu 26/10/23

--- app/src/main/java/com/example/fcinema_app/activities/DoiMatKhauActivity.java
+++ app/src/main/java/com/example/fcinema_app/activities/DoiMatKhauActivity.java
@@ -2,29 +2,106 @@ package com.example.fcinema_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.fcinema_app.R;
+import com.example.fcinema_app.Utils.APIClient;
+import com.example.fcinema_app.Utils.APIInterface;
+import com.example.fcinema_app.models.NguoiDung;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DoiMatKhauActivity extends AppCompatActivity {
 
-    androidx.appcompat.widget.Toolbar mToolbar;
+    private androidx.appcompat.widget.Toolbar mToolbar;
+    private ImageView imgBack;
+    private TextInputEditText edOldPass,edNewPass,edComfirmPass;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_doi_mat_khau);
+        setContentView(R.layout.activity_doi_mat_khau2);
 
-        mToolbar = findViewById(R.id.toolbarDMK);
+        mToolbar = findViewById(R.id.toolbarDMK2);
+        imgBack=findViewById(R.id.imgBackFromChangePass);
+        edOldPass=findViewById(R.id.tnpMatKhauCu);
+        edNewPass=findViewById(R.id.tnpMatKhauMoi2);
+        edComfirmPass=findViewById(R.id.tnpNhapLaiMatKhauMoi2);
 
-        mToolbar.setNavigationIcon(R.drawable.back);
+        imgBack.setOnClickListener(v -> {
+            finish();
+        });
 
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
+        findViewById(R.id.btnSaveChangePass).setOnClickListener(v -> {
+            if(validate()>0) {
+                updatePassword();
             }
         });
+        edOldPass.setText(getMK());
+
+    }
+
+    private void updatePassword(){
+        APIInterface apiInterface= APIClient.getClient().create(APIInterface.class);
+        String newP=edNewPass.getText().toString().trim();
+        NguoiDung nguoiDung=new NguoiDung(newP);
+        Call<Void> call=apiInterface.changeMatKhauNguoiDungByEmail(getEmail(),nguoiDung);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if(response.isSuccessful()) {
+                    Toast.makeText(DoiMatKhauActivity.this, "Đổi mật khẩu thành công", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(DoiMatKhauActivity.this,"Lỗi"+t.getMessage(),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private int validate(){
+        int check=1;
+        String oldP=edOldPass.getText().toString().trim();
+        String newP=edNewPass.getText().toString().trim();
+        String comfirmP=edComfirmPass.getText().toString().trim();
+
+        if(oldP.isEmpty() || newP.isEmpty() || comfirmP.isEmpty()){
+            Toast.makeText(DoiMatKhauActivity.this,"Vui lòng nhập đủ các trường",Toast.LENGTH_SHORT).show();
+            check=-1;
+        }else{
+            if(!oldP.equals(getMK())){
+                Toast.makeText(DoiMatKhauActivity.this, "Mật khẩu cũ không đúng" , Toast.LENGTH_SHORT).show();
+                check=-1;
+            }
+            if(!newP.equals(comfirmP)){
+                Toast.makeText(DoiMatKhauActivity.this, "Xác nhận mật khẩu không trùng" , Toast.LENGTH_SHORT).show();
+                check=-1;
+            }
+        }
+        return check;
+    }
+    private String getEmail(){
+        String email = getIntent().getStringExtra("email");
+        return email;
+    }
+    private String getMK(){
+        String matKhau = getIntent().getStringExtra("matKhau");
+        return matKhau;
     }
 }
